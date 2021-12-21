cfebcda374748126b7c8563671bc3c9dae0660cc
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2021-12-21 11:53:56 -0500

    Added some sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -99,6 +99,7 @@ public class PlayerController : MonoBehaviour
         if (hit.transform.tag == "Obstacle")
         {
             PlayerManager.gameOver = true;
+            FindObjectOfType<AudioManager>().PlaySound("GameOver");
         }
     }
 }
